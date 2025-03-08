c4e9decad9495ec8a1c4eb5cac3b288077d56390
EguJaredoBoyd <eguchidieberejared@gmail.com>
2025-03-08 16:00:54 +0000

    New Commit

---
+++ .history/json-practice/script2_20250308151057.js
@@ -0,0 +1,11 @@
+async function populate() {
+  const requestURL =
+    "./practice.JSON";
+  const request = new Request(requestURL);
+
+  const response = await fetch(request);
+  const superHeroes = await response.json();
+
+  populateHeader(superHeroes);
+  populateHeroes(superHeroes);
+}
